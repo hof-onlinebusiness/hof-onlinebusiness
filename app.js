1ca9bbb3fcc7c891a67c8df6e305ea71887089f4
Jay Arre Toque <jayarre81@gmail.com>
2024-09-14 21:56:31 +0900

    Updated firebase code

--- app.js
+++ app.js
@@ -1,24 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore, doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDZfTvPrbExyZ5UMsRNZ_IPYH4NDe61tM8",
-    authDomain: "hof-online-booking-system.firebaseapp.com",
-    projectId: "hof-online-booking-system",
-    storageBucket: "hof-online-booking-system.appspot.com",
-    messagingSenderId: "513439830348",
-    appId: "1:513439830348:web:356c7a6e2ad5fbfeffbfe9",
-    measurementId: "G-FNQE3GHVDD"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Initialize Firestore
-const db = getFirestore(app);
-
 const form = document.getElementById('bookingForm');
 const statusDiv = document.getElementById('status');
 const limit = 3;  // Max number of bookings per time slot
@@ -35,10 +14,8 @@ form.addEventListener('submit', async (e) => {
     }
 
     const slot = `${date}_${time}`;
-
-    // Get a reference to the document for the specific time slot
-    const slotRef = doc(db, "appointments", slot);
-    const docSnap = await getDoc(slotRef);
+    const slotRef = db.collection('appointments').doc(slot);
+    const docSnap = await slotRef.get();
 
     if (docSnap.exists()) {
         const data = docSnap.data();
@@ -47,12 +24,12 @@ form.addEventListener('submit', async (e) => {
         } else {
             // Add booking
             data.bookings.push({ user: 'anonymous' });  // Replace 'anonymous' with actual user info
-            await updateDoc(slotRef, { bookings: data.bookings });
+            await slotRef.update({ bookings: data.bookings });
             statusDiv.innerText = 'Your appointment is booked!';
         }
     } else {
         // Create new booking slot
-        await setDoc(slotRef, { bookings: [{ user: 'anonymous' }] });  // Replace 'anonymous' with actual user info
+        await slotRef.set({ bookings: [{ user: 'anonymous' }] });  // Replace 'anonymous' with actual user info
         statusDiv.innerText = 'Your appointment is booked!';
     }
 });
